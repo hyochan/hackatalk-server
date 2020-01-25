84f88b616ec322261d1d325b2d3f018ccf123428
hyochan <dooboolab@gmail.com>
2020-01-25 23:05:37 +0900

    x15

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ export const createApp = (): Express => {
     }
   });
   app.get('/', (req, res) => {
-    res.send('It works!!!! production x14');
+    res.send('It works!!!! production x15');
   });
 
   return app;
