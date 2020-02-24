13b5b92301e74930bfc7785362806f12b4d58b0c
daadaadaah <daadaadaah@gmail.com>
2020-02-24 10:19:30 +0900

    Fix circleci error

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,9 +5,9 @@ import Membership, { MembershipModelStatic } from './Membership';
 import Message, { MessageModelStatic } from './Message';
 import Notification, { NotificationModelStatic } from './Notification';
 import Photo, { PhotoModelStatic } from './Photo';
+import Reaction, { ReactionModelStatic } from './Reaction';
 import Reply, { ReplyModelStatic } from './Reply';
 import User, { UserModelStatic } from './User';
-import Reaction, { ReactionModelStatic } from './Reaction';
 
 export default {
   User,
