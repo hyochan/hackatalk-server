970dc8537196b14591f2132bfaed04aa8e1faff3
hyochan <dooboolab@gmail.com>
2020-02-27 00:47:16 +0900

    Azure storage integration

--- src/resolvers/file.ts
+++ src/resolvers/file.ts
@@ -1,10 +1,23 @@
 import { File, Resolvers } from '../generated/graphql';
 
+import { uploadFileToAzureBlobFromFile } from '../utils/azure';
+
 const resolver: Resolvers = {
   Mutation: {
-    singleUpload: async (_, args): Promise<File> => {
+    singleUpload: async (_, args): Promise<string> => {
+      const dir: string = args.dir ? args.dir : 'defaults';
       const file = await args.file;
-      return file;
+      const resultUpload = await uploadFileToAzureBlobFromFile(
+        `./files/${file.filename}`,
+        file.filename,
+        dir,
+      );
+
+      const {
+        STORAGE_ENDPOINT,
+      } = process.env;
+
+      return `${STORAGE_ENDPOINT}/${dir}/${file.filename}`;
     },
   },
 };
