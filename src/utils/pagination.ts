9aba5c631015d0bd09297b1197f549fdd0436dbd
TaeSeong Park <imf4@naver.com>
2020-03-08 03:40:29 +0900

    Remove unused methods

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,54 +1,5 @@
-import {
-  Message,
-  MessagesConnection,
-  PageInfo,
-  Scalars,
-  User,
-  UsersConnection,
-} from '../generated/graphql';
+import { Message, PageInfo, User } from '../generated/graphql';
 
-interface ResultConnection<Schema, TypeName> {
-  __typename?: TypeName;
-  cursor?: Scalars['String'];
-  hasMore: Scalars['Boolean'];
-  results: Array<Schema>;
-}
-function paginateResults<
-  Schema extends User | Message,
-  Connection extends UsersConnection | MessagesConnection,
-  TypeName
->({
-  after,
-  pageSize = 20,
-  lastRow,
-  results,
-}: {
-  after?: string;
-  pageSize?: number;
-  lastRow: Schema;
-  results: Array<Schema>;
-}): ResultConnection<Schema, TypeName> {
-  if (results.length === 0) {
-    return {
-      cursor: null,
-      hasMore: false,
-      results,
-    };
-  }
-  const resultLastRowCreatedAt = results[results.length - 1].createdAt;
-  const resultLastRowCreatedAtDt = new Date(resultLastRowCreatedAt).getTime();
-  const paramLastRowCreatedAt = lastRow.createdAt;
-  const paramLastRowCreatedAtDt = new Date(paramLastRowCreatedAt).getTime();
-  const isLast = resultLastRowCreatedAtDt === paramLastRowCreatedAtDt;
-  const cursor = isLast ? null : resultLastRowCreatedAt;
-  const hasMore = !isLast;
-
-  return {
-    cursor,
-    hasMore,
-    results,
-  };
-}
 function getPageInfo<Schema extends User | Message>({
   first,
   last,
@@ -96,4 +47,4 @@ function getPageInfo<Schema extends User | Message>({
   return pageInfo;
 }
 
-export { paginateResults, getPageInfo };
+export { getPageInfo };
