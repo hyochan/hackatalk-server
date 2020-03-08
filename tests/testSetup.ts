c14e72c38ac71538d1a2218659ece7c517d87716
TaeSeong Park <imf4@naver.com>
2020-03-08 19:15:10 +0900

    Add getClientUserSignedIn function

--- tests/testSetup.ts
+++ tests/testSetup.ts
@@ -1,3 +1,5 @@
+import { GraphQLClient, request } from 'graphql-request';
+
 import { Http2Server } from 'http2';
 import { createApp } from '../src/app';
 import express from 'express';
@@ -6,7 +8,20 @@ import { startServer } from '../src/server';
 
 const port = 4000;
 let server: Http2Server;
+
 export const testHost = `http://localhost:${port}/graphql`;
+export const getClientUserSignedIn = async (
+  testHost: string,
+  signInEmail: string,
+): Promise<GraphQLClient> => {
+  const signInRes = await request(testHost, signInEmail);
+  const client = new GraphQLClient(testHost, {
+    headers: {
+      authorization: `Bearer ${signInRes.signInEmail.token}`,
+    },
+  });
+  return client;
+};
 
 beforeAll(async () => {
   const app: express.Application = createApp();
