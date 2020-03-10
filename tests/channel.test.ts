6f0b47d93ea5344a511914243798529ce6ec971e
smallbee <smallbee3@gmail.com>
2020-03-11 00:51:26 +0900

    Add 'channel' test case for having a newly created message

--- tests/channel.test.ts
+++ tests/channel.test.ts
@@ -6,23 +6,23 @@ describe('Resolver - Channel', () => {
   let client: GraphQLClient;
   const testUserIds = [];
   const signUpUser = /* GraphQL */`
-  mutation signUp($user: UserInput!) {
-    signUp(user: $user) {
-      token,
-      user {
-        id
-        email
+    mutation signUp($user: UserInput!) {
+      signUp(user: $user) {
+        token,
+        user {
+          id
+          email
+        }
       }
     }
-  }
-`;
+  `;
 
   beforeAll(async () => {
     const { signUp } = await request(testHost, signUpUser, {
       user: {
-        email: 'test-1@dooboolab.com',
+        email: 'test1@dooboolab.com',
         password: 'password',
-        name: 'test-1',
+        name: 'test1',
       },
     });
     client = new GraphQLClient(testHost, {
@@ -33,16 +33,24 @@ describe('Resolver - Channel', () => {
 
     const { signUp: signUpUser2 } = await request(testHost, signUpUser, {
       user: {
-        email: 'test-2@dooboolab.com',
+        email: 'test2@dooboolab.com',
         password: 'password',
-        name: 'test-2',
+        name: 'test2',
       },
     });
-
     testUserIds.push(signUpUser2.user.id);
+
+    const { signUp: signUpUser3 } = await request(testHost, signUpUser, {
+      user: {
+        email: 'test3@dooboolab.com',
+        password: 'password',
+        name: 'test3',
+      },
+    });
+    testUserIds.push(signUpUser3.user.id);
   });
 
-  const mutation = /* GraphQL */`
+  const createChannel = /* GraphQL */`
     mutation createChannel($channel: ChannelInput){
       createChannel(channel: $channel) {
         id
@@ -60,7 +68,7 @@ describe('Resolver - Channel', () => {
       },
     };
 
-    const promise = request(testHost, mutation, variables);
+    const promise = request(testHost, createChannel, variables);
     expect(promise).rejects.toThrow();
   });
 
@@ -72,7 +80,7 @@ describe('Resolver - Channel', () => {
       },
     };
 
-    const promise = client.request(mutation, variables);
+    const promise = client.request(createChannel, variables);
     expect(promise).rejects.toThrow();
   });
 
@@ -83,9 +91,69 @@ describe('Resolver - Channel', () => {
         name: 'test-channel',
       },
     };
-    const promise = client.request(mutation, variables);
+    const promise = client.request(createChannel, variables);
     expect(promise).resolves.toHaveProperty('createChannel.id');
     expect(promise).resolves.toHaveProperty('createChannel.name');
     expect(promise).resolves.toHaveProperty('createChannel.type');
+  });
+
+  const channels = /* GraphQL */`
+    query {
+      channels {
+        id
+        name
+        messages {
+          id
+          type
+          text
+        }
+      }
+    }
+  `;
+
+  const createMessage = /* GraphQL */`
+    mutation createMessage($message: String!, $users: [String!]!, $channelId: String) {
+      createMessage(
+        message: $message
+        users: $users
+        channelId: $channelId
+      ) {
+        channelId
+        message {
+            id
+            type
+            text
+            filePath
+        }
+      }
+    }
+  `;
+
+  it('should have messages as nested data after create message', async () => {
+    const variables = {
+      channel: {
+        friendIds: [...testUserIds],
+        name: 'test-channel2',
+      },
+    };
+    const response = await client.request(createChannel, variables);
+
+    const TEXT = 'Do you like kimchi?';
+    const variables2 = {
+      message: TEXT,
+      users: [...testUserIds],
+      channelId: response.createChannel.id,
+    };
+    const response2 = await client.request(createMessage, variables2);
+    expect(response).toHaveProperty('createChannel.id');
+    expect(response2.createMessage.channelId).toEqual(response.createChannel.id);
+
+    const response3 = await client.request(channels);
+    response3.channels.forEach((channel) => {
+      if (channel.id === response.createChannel.id) {
+        expect(channel.messages[0].id).toEqual(response2.createMessage.message.id);
+        expect(channel.messages[0].text).toEqual(TEXT);
+      }
+    });
   });
 });
