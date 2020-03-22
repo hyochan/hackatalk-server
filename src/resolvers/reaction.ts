102acbf00b426ab0ff5922228e0fd20d22ffa93d
dahee kwak <daheekwak@daheeui-MacBookPro.local>
2020-03-22 16:54:09 +0900

    Create Reaction Resolver

--- src/resolvers/reaction.ts
+++ src/resolvers/reaction.ts
@@ -19,59 +19,21 @@ const resolver: Resolvers = {
 
       return reaction;
     },
+    deleteReaction: async (_, { reactionId }, { verifyUser, models }): Promise<number> => {
+      const auth = verifyUser();
+      checkAuth(auth);
 
+      const { Reaction: reactionModel } = models;
+
+      const result = await reactionModel.destroy({
+        where: {
+          id: reactionId,
+        },
+      });
+      
+      return result;
+    },
   },
-
-  //   // TODO : 논의 필요! Query 완성 -> Migration 완성 -> Test Code 완성 -> PR 날리기
-  //   // Query: {
-  //   //   reactions: async (_, { messageId}, { verifyUser, models }): Promise<Reaction[]> => {
-  //   //     // const auth = verifyUser();
-  //   //     // checkAuth(auth);
-
-  //   //     const { Reaction: reactionModel } = models;
-  //   //     return reactionModel.findAll({
-  //   //       where: {
-  //   //         messageId,
-  //   //       },
-  //   //     });
-  //   //     return reactionModel.findAll();
-  //   //   },
-  //   // },
-
-  //   Mutation: {
-  //     // reactionId
-  //     createReaction: async (_, { messageId, type }, { verifyUser, models }): Promise<Reaction> => {
-  //       // const auth = verifyUser();
-  //       // checkAuth(auth);
-
-  //       // if (!auth) throw new AuthenticationError('User is not signed in');
-
-  //       const { Reaction: reactionModel } = models;
-  //       const reaction = await reactionModel.create({
-  //         messageId,
-  //         // userId: auth.userId,
-  //         type,
-  //       });
-
-  //       return reaction;
-  //     },
-  //     deleteReaction: async (_, { reactionId }, { verifyUser, models }): Promise<number> => {
-  //       // const auth = verifyUser();
-  //       // checkAuth(auth);
-
-  //       // if (!auth) throw new AuthenticationError('User is not signed in');
-
-  //       const { Reaction: reactionModel } = models;
-
-  //       const result = await reactionModel.destroy({
-  //         where: {
-  //           id: reactionId,
-  //         },
-  //       });
-
-//       return result;
-//     },
-//   },
 };
 
 export default resolver;
