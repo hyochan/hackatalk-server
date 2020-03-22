8b9f2d5f79877f044c95ae4fed18e5472f53d6e2
dahee kwak <daheekwak@daheeui-MacBookPro.local>
2020-03-22 21:06:41 +0900

    Create test code for Reaction Resolver

---
+++ tests/reaction.test.ts
@@ -0,0 +1,126 @@
+import { GraphQLClient, request } from 'graphql-request';
+
+import { testHost } from './testSetup';
+import { Json } from 'sequelize/types/lib/utils';
+
+describe('Resolver - Reaction', () => {
+  let client: GraphQLClient;
+  const testUserIds = [];
+  const signUpUser = /* GraphQL */`
+  mutation signUp($user: UserInput!) {
+      signUp(user: $user) {
+        token,
+        user {
+            id
+            email
+        }
+      }
+  }
+  `;
+
+  beforeAll(async () => {
+    const { signUp } = await request(testHost, signUpUser, {
+      user: {
+        email: 'test1@dooboolab.com',
+        password: 'password',
+        name: 'test1',
+      },
+    });
+    client = new GraphQLClient(testHost, {
+      headers: {
+        authorization: signUp.token,
+      },
+    });
+
+    const { signUp: signUpUser2 } = await request(testHost, signUpUser, {
+      user: {
+        email: 'test2@dooboolab.com',
+        password: 'password',
+        name: 'test2',
+      },
+    });
+
+    testUserIds.push(signUpUser2.user.id);
+
+  });
+
+  const createChannel = /* GraphQL */`
+    mutation createChannel($channel: ChannelInput){
+      createChannel(channel: $channel) {
+        id
+        type
+        name
+      }
+    }
+  `;
+
+  const createMessage = /* GraphQL */`
+    mutation createMessage($message: String!, $channelId: String!) {
+      createMessage(
+        message: $message
+        channelId: $channelId
+      ) {
+        channelId
+        message {
+          id
+        }
+      }
+    }
+  `;
+
+  const createReaction = /* GraphQL */`
+    mutation createReaction($type: String!, $messageId: ID!) {
+      createReaction(
+        messageId: $messageId 
+        type: $type
+      ) {
+        id
+      }
+    } 
+  `;
+
+  it('should create reaction', async () => {
+    const channelVariables = {
+      channel: {
+        friendIds: testUserIds,
+        name: 'test-channel',
+      },
+    };
+    const channelResponse = await client.request(createChannel, channelVariables);
+
+    const messageVariables = {
+      message: 'hello hello!!',
+      channelId: channelResponse.createChannel.id,
+    };
+    const messageResponse = await client.request(createMessage, messageVariables);
+
+    const reactionVariables = {
+      messageId: messageResponse.createMessage.message.id,
+      type: 'smile',
+    };
+    const response = await client.request(createReaction, reactionVariables);
+
+    expect(response).toHaveProperty('createReaction');
+    expect(response.createReaction).toHaveProperty('id');
+    expect(response.createReaction.id).not.toBeNull();
+
+  });
+
+
+  const deleteReaction = /* GraphQL */`
+    mutation deleteReaction($reactionId: ID!) {
+      deleteReaction(reactionId: $reactionId) 
+    }
+  `;
+
+  it('should delete reaction', async () => {    
+    const variables = {
+      reactionId: 'test',
+    };
+    const response = await client.request(deleteReaction, variables);
+
+    expect(response).toHaveProperty('deleteReaction');
+    expect(response.deleteReaction).toBe(0);
+  
+  });
+});
